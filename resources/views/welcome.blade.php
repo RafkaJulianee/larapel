ec95bfa23937b2b3b4ab64cbee5a8ca85a68cf52
RafkaJulianee <rafkagenggkk@gmail.com>
2026-01-20 20:13:35 +0700

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel.Com</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <style>
+        body{
+            font-family: sans-serif;
+
+        }
+
+        .navbar a{
+            text-decoration: none;
+            color: black;
+            padding: 20px;
+            margin-top: 30px;
+
+
+
+        
+            
+
+            
+            
+        }
+
+
+    </style>
+</head>
+<body>
+    <div class="navbar">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+
+
+    </div>
+
+    
+</body>
+</html>

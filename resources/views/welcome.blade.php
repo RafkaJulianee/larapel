16398d37fc0e5f88ed456f9f1f352fd7bc789d2b
RafkaJulianee <rafkagenggkk@gmail.com>
2026-01-24 08:28:48 +0700

    UP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,28 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <style>
-        body{
-            font-family: sans-serif
-        }
-        h1{
-            text-align: center;
-
-        }
-        a{
-            text-decoration: none;
-            display: inline;
-            color: black;
-
-
-            
-        }
-    </style>
 </head>
 <body>
-    <a href="">Home</a>
-    <a href="">About</a>
-    <a href="">Contact</a>
-    <h1>Hello World</h1>
+    <script>
+        
+    </script>
 </body>
 </html>

509824c0e800b756f6ca0dcf659f02e16b5528c2
RafkaJulianee <rafkagenggkk@gmail.com>
2026-01-26 10:30:16 +0700

    up

--- app/Models/Siswa.php
+++ app/Models/Siswa.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
-    //
+    protected $table = 'siswa';
+    protected $fillable = [
+        'kelas_id',
+        'nis',
+        'nama_siswa',
+        'jenis_kelamin',
+        'tanggal_lahir',
+        'alamat',
+        'email',
+        'no_telepon',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class,'kelas_id');
+    }
 }

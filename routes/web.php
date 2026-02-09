6e8f5bd0fe42e151f4d0f9d79cfbe036fb311512
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-09 20:03:53 +0700

    feat: Add `Kelas` and `Siswa` resource routes and a new `/dominik` route with its corresponding view.

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\KelasConstroller;
 use App\Http\Controllers\SiswaController;
-use App\Http\Controllers\ShopController;
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,5 +15,8 @@ Route::post('/kelas', [KelasConstroller::class, 'store'])->name('kelas.store');
 Route::get('/kelas/{id}/edit', [KelasConstroller::class, 'edit'])->name('kelas.edit');
 Route::put('/kelas/{id}', [KelasConstroller::class, 'update'])->name('kelas.update');
 Route::get('/kelas/{id}', [KelasConstroller::class, 'destroy'])->name('kelas.destroy');
-Route::resource('siswa', SiswaController::class);
+Route::resource('siswa', SiswaController::class);       
 
+Route::get('/dominik', function () {
+    return view('latihan.dominik');
+});

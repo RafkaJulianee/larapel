abbd68d51f6f1a819c5151c69af73230678c3ff5
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-14 21:52:33 +0700

    up

--- resources/views/latihan/dominik.blade.php
+++ resources/views/latihan/dominik.blade.php
@@ -3,10 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Apa Aja</title>
+    <style>
+        body{
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        }
+    </style>
 </head>
 <body>
-    <h1>Hello Dominik Szobozlai</h1>
-    <p></p>
+    
 </body>
 </html>

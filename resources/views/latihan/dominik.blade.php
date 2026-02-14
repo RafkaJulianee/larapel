8735e7f454473210550f4e7aa9becf90024731af
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-14 21:57:19 +0700

    up

--- resources/views/latihan/dominik.blade.php
+++ resources/views/latihan/dominik.blade.php
@@ -8,9 +8,32 @@
         body{
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
+
+        .navbar{
+            background-color: #333;
+            overflow: hidden;
+        }
+
+        .navbar a{
+            float: left;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+            font-size: 17px;
+        }
+
+        .navbar a:hover{
+            background-color: #ddd;
+            color: black;
+        }       
     </style>
 </head>
 <body>
-    
+    <div class="navbar">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+    </div>
 </body>
 </html>

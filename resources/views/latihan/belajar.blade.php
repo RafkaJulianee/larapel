52f9f431ea225426e9eca3b92d49caace7b7841e
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-15 21:07:22 +0700

    up

--- resources/views/latihan/belajar.blade.php
+++ resources/views/latihan/belajar.blade.php
@@ -5,11 +5,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belajar Css</title>
     <style>
+        body{
+            background-image: url('img/Artemis.jpg');
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            height: 100vh;
+            width: 100%;    
+        }
+
+        h1{
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+             text-decoration: none;
+            font-size: 100px;
+        }
+        h1 :hover{
+            
+        }
 
     </style>
 </head>
 <body>
 
-    <h1></h1>
+    <h1>ARTEMIS</h1>
 </body>
 </html>

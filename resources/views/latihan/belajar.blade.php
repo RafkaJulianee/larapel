80098edf7214eed0f259f1e64c68359c26ce04b8
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-15 21:32:47 +0700

    up

--- resources/views/latihan/belajar.blade.php
+++ resources/views/latihan/belajar.blade.php
@@ -1,37 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Belajar Css</title>
-    <style>
-        body{
-            background-image: url('img/Artemis.jpg');
-            background-size: cover;
-            background-position: center;
-            background-repeat: no-repeat;
-            height: 100vh;
-            width: 100%;    
-        }
-
-        h1{
-            color: white;
-            text-align: center;
-            padding: 14px 16px;
-             text-decoration: none;
-            font-size: 100px;
-        }
-        h1 :hover{
-            background-color: red       ;
-            color: white;
-            border-radius: 10px;
-            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2); 
-        }
-
-    </style>
+<style>
+body {
+  background: #ffffff url("img/samurai.png") no-repeat right top;
+  margin-right: 200px;
+  font-family: sans-serif;
+}
+</style>
 </head>
 <body>
 
-    <h1>ARTEMIS</h1>
+<h1>The backgroud Property</h1>
+
+<p>The background property is a shorthand property for specifying all the background properties in one declaration.</p>
+
+<p>Here, the background image is only shown once, and it is also positioned in the top-right corner.</p>
+
+<p>We have also added a right margin, so that the text will not write over the background image.</p>
+
 </body>
 </html>
+
+

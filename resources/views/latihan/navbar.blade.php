8b763ac92d6dcf174f410abd26eb549fa45ad2a3
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-19 21:34:26 +0700

    up

--- resources/views/latihan/navbar.blade.php
+++ resources/views/latihan/navbar.blade.php
@@ -6,6 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<style>
+    ul{
+        list-style: none;
+        display: flex;
+        gap: 10px;
+    }   
+    a{
+        text-decoration: none;
+        color: black;
+    }   
+    a:hover{
+        color: blue;
+    }       
+    nav{
+        background-color: #f1f1f1;
+        padding: 10px;
+    }       
+    
+    
+</style>    
 <body>
    <nav>
     <ul>

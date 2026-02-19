955130c12662cc92504a422399199a37645b0fb5
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-19 21:37:22 +0700

    up

--- resources/views/latihan/navbar.blade.php
+++ resources/views/latihan/navbar.blade.php
@@ -7,32 +7,11 @@
     <title>Document</title>
 </head>
 <style>
-    ul{
-        list-style: none;
-        display: flex;
-        gap: 10px;
-    }   
-    a{
-        text-decoration: none;
-        color: black;
-    }   
-    a:hover{
-        color: blue;
-    }       
-    nav{
-        background-color: #f1f1f1;
-        padding: 10px;
-    }       
-    
+       
+
     
 </style>    
 <body>
-   <nav>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-   </nav>   
+   
 </body>
 </html>

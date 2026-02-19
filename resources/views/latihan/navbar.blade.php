9bdb183767bba8280d971a683af7832bcaeb6496
RafkaJulianee <rafkagenggkk@gmail.com>
2026-02-19 21:37:37 +0700

    up

--- resources/views/latihan/navbar.blade.php
+++ resources/views/latihan/navbar.blade.php
@@ -6,12 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<style>
-       
-
     
-</style>    
 <body>
-   
+   <nav style="background:black; padding:10px; color:white;">
+    <h2>{{ $title }}</h2>
+
+    <ul style="display:flex; gap:15px; list-style:none;">
+        <li><a href="/" style="color:white;">Home</a></li>
+        <li><a href="/about" style="color:white;">About</a></li>
+        <li><a href="/contact" style="color:white;">Contact</a></li>
+    </ul>
+</nav>
+
 </body>
 </html>

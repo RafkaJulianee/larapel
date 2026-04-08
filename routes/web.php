e438e46097a24dd279de062903c0cac5bc81a6e2
RafkaJulianee <rafkagenggkk@gmail.com>
2026-04-08 21:04:18 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@ use App\Http\Controllers\SiswaController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
 Route::get('/kelas', [KelasConstroller::class, 'index'])->name('kelas.index');
 Route::get('/kelas/create', [KelasConstroller::class, 'create'])->name('kelas.create');
 Route::post('/kelas', [KelasConstroller::class, 'store'])->name('kelas.store');
@@ -16,7 +20,4 @@ Route::get('/kelas/{id}/edit', [KelasConstroller::class, 'edit'])->name('kelas.e
 Route::put('/kelas/{id}', [KelasConstroller::class, 'update'])->name('kelas.update');
 Route::get('/kelas/{id}', [KelasConstroller::class, 'destroy'])->name('kelas.destroy');
 Route::resource('siswa', SiswaController::class);
-
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-
 
